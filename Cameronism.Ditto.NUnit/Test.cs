2fe678648a2b4ba2dbfe0490e6d78eb4383f40f9
Cameron Jordan <cjordan@cameronism.com>
2013-04-26 08:42:18 -0700

    Switch to NUnit for monodevelop

--- Cameronism.Ditto.NUnit/Test.cs
+++ Cameronism.Ditto.NUnit/Test.cs
@@ -1,12 +1,11 @@
-﻿using System;
+using System;
+using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using Xunit;
-using Cameronism;
 
-namespace Cameronism.Tests
+namespace Cameronism.DittoNUnit
 {
+	[TestFixture]
 	public class Basics
 	{
 		class MyType
@@ -15,11 +14,11 @@ namespace Cameronism.Tests
 			public string B;
 		}
 
-		[Fact]
+		[Test]
 		public void Readme()
 		{
 			var myObject = new MyType();
-
+			
 			// static usage
 			var copy = Ditto.DeepClone(myObject);
 			Assert.False(Object.ReferenceEquals(copy, myObject), "copy is not the same object");
@@ -35,60 +34,61 @@ namespace Cameronism.Tests
 			Assert.True(comparer.Equals(copy2, myObject), "copy2 is equivalent");
 			Assert.True(comparer.GetHashCode(copy2) == Ditto.GetHashCode(myObject));
 		}
-
-		[Fact]
+		
+		[Test]
 		public void RealStuff()
 		{
 			var myObject = new MyType { A = 42 };
-
+			
 			var copy = Ditto.DeepClone(myObject);
 			Assert.NotNull(copy);
-			Assert.Equal(42, copy.A);
-
+			Assert.AreEqual(42, copy.A);
+			
 			copy = Ditto.DeepClone<MyType>(null);
 			Assert.Null(copy);
 		}
-
+		
 		class Outer
 		{
 			public MyType Instance;
 		}
-
-		[Fact]
+		
+		[Test]
 		public void Deeper()
 		{
 			var obj = new Outer { Instance = new MyType { B = "the answer", A = 42 } };
 			var copy = Ditto.DeepClone(obj);
-			Assert.NotSame(obj.Instance, copy.Instance);
-			Assert.Same(obj.Instance.B, copy.Instance.B);
-
+			Assert.AreNotSame(obj.Instance, copy.Instance);
+			Assert.AreSame(obj.Instance.B, copy.Instance.B);
+			
 			Assert.Null(Ditto.DeepClone(new Outer()).Instance);
 		}
-
+		
 		class PropContainer<T>
 		{
 			public T Prop { get; set; }
 		}
-
-		[Fact]
+		
+		[Test]
 		public void Properties()
 		{
 			var obj = new PropContainer<int> { Prop = 42 };
 			var clone = Ditto.DeepClone(obj);
-			Assert.Equal(obj.Prop, clone.Prop);
+			Assert.AreEqual(obj.Prop, clone.Prop);
 		}
-
-		[Fact]
+		
+		[Test]
 		public void CommonInterfaces()
 		{
 			var obj = new PropContainer<IEnumerable<int>> { Prop = Enumerable.Range(0, 10) };
 			var clone = Ditto.DeepClone(obj);
-			Assert.Equal(obj.Prop, clone.Prop);
-			Assert.NotSame(obj.Prop, clone.Prop);
-
+			Assert.AreEqual(obj.Prop, clone.Prop);
+			Assert.AreNotSame(obj.Prop, clone.Prop);
+			
 			obj = new PropContainer<IEnumerable<int>> { Prop = null };
 			clone = Ditto.DeepClone(obj);
 			Assert.Null(clone.Prop);
 		}
 	}
 }
+
